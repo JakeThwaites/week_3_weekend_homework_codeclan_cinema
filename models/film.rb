0ec67e61c2d9f1fe61e753d62b44246daaa212a1
Jake Thwaites <jakethwaites@hotmail.co.uk>
2018-11-02 15:19:15 +0000

    completed CRUD methods

--- models/film.rb
+++ models/film.rb
@@ -11,10 +11,43 @@ class Film
     @id = options['id'] if options['id']
   end
 
+  def all()
+    sql = "SELECT * FROM films"
+    SqlRunner.run(sql)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM films"
+    SqlRunner.run(sql)
+  end
+
   def save()
     sql = "INSERT INTO films (title, price) VALUES ($1, $2) RETURNING id"
     values = [@title, @price]
     film = SqlRunner.run(sql, values)[0]
     @id = film['id'].to_i
+  end
+
+  def delete()
+    sql = "DELETE FROM films WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def update()
+    sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3;"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def customers()
+    sql = "SELECT customers.*
+  FROM customers
+  INNER JOIN tickets
+  ON customers.id = tickets.customer_id
+  WHERE film_id = $1"
+    values = [@id]
+    customers = SqlRunner.run(sql, values)
+    return customers.map { |customer| Customer.new(customer) }
   end
 end
--- models/ticket.rb
+++ models/ticket.rb
@@ -11,10 +11,33 @@ class Ticket
     @id = options['id'] if options['id']
   end
 
+  def self.all()
+    sql = "SELECT * FROM tickets"
+    SqlRunner.run(sql)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM tickets"
+    SqlRunner.run(sql)
+  end
+
   def save()
     sql = "INSERT INTO tickets (customer_id, film_id) VALUES ($1, $2) RETURNING id"
     values = [@customer_id, @film_id]
     ticket = SqlRunner.run(sql, values)[0]
     @id = ticket['id'].to_i
   end
+
+  def delete()
+    sql = "DELETE FROM tickets WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def update()
+    sql = "UPDATE tickets SET (customer_id, film_id) = ($1, $2) WHERE id = $3"
+    values = [@customer_id, @film_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
 end
